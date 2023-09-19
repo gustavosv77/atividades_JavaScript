820915e4bd9dc3e0753100f95ed4134ba17b3853
gustavosv77 <143461595+gustavosv77@users.noreply.github.com>
2023-09-19 13:47:20 -0300

    calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,41 +1,45 @@
-const prompt = require("prompt-sync")()
+const prompt = require('prompt-sync')();
+
 
 let fator1, fator2, result
 
-console.log("Calculadora Simples!\n")
+console.log("\nCalculadora Simples!\n")
 console.log("\nDigite o primeiro fator:\n\n")
 console.log("Resposta: ")
-prompt(fator1)
+fator1 = parseFloat(prompt(fator1))
+console.clear()
 console.log("\nDigite o segundo fator:\n\n")
 console.log("Resposta: ")
-prompt(fator2)
+fator2 = parseFloat(prompt(fator2))
+console.clear()
 
 console.log("\nEscolha uma operação:\n")
 console.log("1 - ADIÇÃO\t\t 2 - SUBTRAÇÃO\n")
 console.log("3 - MULTIPLICAÇÃO\t 4 - DIVISÃO\n\n")
 console.log("Resposta: ")
-opcao=parseFloat(prompt())
+opcao=parseInt(prompt())
+console.clear()
 
 
 
 if (opcao == 1) {	 
-    result=parseFloat(fator1() + fator2())
-    console.log(result)
+    result= fator1 + fator2
+    console.log(`\nO resultado da Adição é: ${result} \n`)
   }
 
   if (opcao == 2) {  	    
     result = fator1 - fator2
-    console.log("\nO resultado da Subtração é: ", result, "\n")
+    console.log(`\nO resultado da Subtração é: ${result} \n`)
   }
 
   if (opcao == 3) { 	
     result = fator1 * fator2
-    console.log("\nO resultado da Multiplicação é: \n")
+    console.log(`\nO resultado da Multiplicação é: ${result} \n`)
   }
 
   if (opcao == 4) {   	 
     result = fator1 / fator2
-    console.log("\nO resultado da Divisão é: ", result, "\n")
+    console.log(`\nO resultado da Divisão é: ${result} \n`)
   }
     
 
