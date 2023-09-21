f205c8c1ecba776c96bc7af55210bcc3331b4fcf
gustavosv77 <gustavosv.senai77@gmail.com>
2023-09-21 19:17:07 -0300

    adicionando a função de reiniciar

--- jurosCompostos.js
+++ jurosCompostos.js
@@ -1,27 +1,46 @@
 const prompt = require('prompt-sync')();
 	
+    let novoCalculo=""
 
-    let taxa , porcentagem , tempo , montante , capital , juros , base , resultado    
-      
 
-      console.log("\n\nCalculadora de Juros Compostos!\n")
-      console.log("\nDigite o valor do Capital:\n")
-      capital = parseFloat(prompt(capital))
-      console.clear()
-      console.log("\nDigite o valor da taxa ao ano em porcentagem:\n")
-      porcentagem = parseFloat(prompt(porcentagem))
-      console.clear()
-      console.log("\nDigite o tempo em anos: ; ex: se for 24 meses informe 2 anos\n")
-      tempo = parseFloat(prompt(tempo))
-      console.clear()
+        function reiniciar(){							
+            console.log("\nDeseja calcular novamente?\n\n")
+            console.log("Digite 'S' para sim ou qualquer tecla para sair:\n\n")      
+            novoCalculo= (prompt())     
+                console.clear()
+                if(novoCalculo =="S" || novoCalculo =="s" || novoCalculo =="Sim" || novoCalculo =="sim"){
+                console.clear()
+                composto()     	    	      	
+                }else{
+                }
+        } 
 
-		taxa = porcentagem / 100
+        function composto(){
+            
+            let taxa , porcentagem , tempo , montante , capital , juros , base , resultado 
 
-        montante =  capital * (taxa+1) **tempo
+            console.log("\n\nCalculadora de Juros Compostos!\n")
+            console.log("\nDigite o valor do Capital:\n")
+            capital = parseFloat(prompt(capital))
+            console.clear()
+            console.log("\nDigite o valor da taxa ao ano em porcentagem:\n")
+            porcentagem = parseFloat(prompt(porcentagem))
+            console.clear()
+            console.log("\nDigite o tempo em anos: ; ex: se for 24 meses informe 2 anos\n")
+            tempo = parseFloat(prompt(tempo))
+            console.clear()
 
-        console.log(`\n\nO montante é de: ${montante.toFixed(2)} R$\n`)
+            taxa = porcentagem / 100
 
-        juros= montante - capital
+            montante =  capital * (taxa+1) **tempo
 
-        console.log(`\nO total de juros foi de: ${juros.toFixed(2)} R$`)
-  
+            console.log(`\n\nO montante é de: ${montante.toFixed(2)} R$\n`)
+
+            juros= montante - capital
+
+            console.log(`\nO total de juros foi de: ${juros.toFixed(2)} R$`)
+        
+            reiniciar()
+        }
+        composto()
+        
