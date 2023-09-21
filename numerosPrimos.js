4a53510e166b3d8f9b4ec36c762b6eaed0dedf28
gustavosv77 <gustavosv.senai77@gmail.com>
2023-09-21 19:51:18 -0300

    reiniciar adicionado

--- numerosPrimos.js
+++ numerosPrimos.js
@@ -1,10 +1,24 @@
 const prompt = require('prompt-sync')();
 	
+        let novoCalculo=""
 
+        function reiniciar(){							
+        console.log("\nDeseja calcular novamente?\n\n")
+        console.log("Digite 'S' para sim ou qualquer tecla para sair:\n\n")      
+        novoCalculo= (prompt())     
+        console.clear()
+            if(novoCalculo =="S" || novoCalculo =="s" || novoCalculo =="Sim" || novoCalculo =="sim"){
+            console.clear()
+                procurar()     	    	      	
+                    }else{
+                    }
+                } 
+
+
+
+        function procurar(){
 	
-	
-		let x , divisores = 0 , valor = 1
-		
+		    let x , divisores = 0 , valor = 1
 
 			console.log("Digite um valor maior que 0 para saber se ele é um número primo!\n")
 			console.log("Digite o valor:\n")
@@ -27,4 +41,7 @@ const prompt = require('prompt-sync')();
 			else{
 			console.log("O número ", x , " não é primo!\n")
 			}
-				}		     
+				}	
+                reiniciar()	     
+            }
+            procurar()
